54af78536797972ca61d8cc6f60084b08de80109
Pierangelo Calanna <pierangelo.calanna@altiusdata.com>
2020-05-06 13:11:01 +0100

    Added storage account and container

--- main.tf
+++ main.tf
@@ -6,6 +6,26 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
     name     = "myTFResourceGroup"
-    location = "westus"
+    location = "West Europe"
 }
 
+resource "azurerm_storage_account" "processinglayers" {
+  name                     = "processinglayers"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+//  tags                     = "${local.tags}"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+//  access_tier              = "Hot"
+//  enable_https_traffic_only = true
+  depends_on               = [azurerm_resource_group.rg]
+
+}
+
+resource "azurerm_storage_container" "raw" {
+  name                  = "raw"
+  storage_account_name  = azurerm_storage_account.processinglayers.name
+  container_access_type = "private"
+  depends_on = [azurerm_storage_account.processinglayers]
+}
